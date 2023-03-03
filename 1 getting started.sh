1fbc0f0dcf5aec730920e76cb13c80bea52f6182
singhxayush <fanduayush2000@gmail.com>
2023-03-04 01:45:31 +0530

    x

---
+++ 1 getting started.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+# to print the date and time
+# date
+
+# to display calender
+# cal
+
+# who r the users
+# who
+
+# to view all the process that u have created 
+# ps
+
+# the ls command
+# to list all the files and directory in ASCII Collating sequence
+# ls
+
+# to list all files with the given keyword as prefix
+# ls keyword*
+# example
+# ls ~/solve/1*
+
+# to count to the numbers of lines in a file
+# output in order - line numbers - total words - total characters - filename
+# wc 1.bash
+
+
+# to create pipeline btn ls and wc to print number of files of the current directory
+# it basically pipelines the ouptput of ls 
+# ls | wc
+
+# assing value to a variable and print
+x=4
+echo $x
+
+# man [command name] -> is used to get an indepth manual for that command
+# man cat
+
+echo 1
