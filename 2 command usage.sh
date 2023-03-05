060a890dfe95c5bb91f4bf7722aa8410f231bffe
singhxayush <fanduayush2000@gmail.com>
2023-03-05 16:21:28 +0530

    x

---
+++ 2 command usage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# to use multiple commands in a single command line
+# (wc forest.txt ; ls -l forest.txt) > op.txt
+
+# to read a manual of a command
+# man wc
+
+# to change password
+# passwd
+
+wc
---
+++ takeargs.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(int argc, char *argv[])
+{
+    if(argc == 2) std::cout<<"T\n";
+    else std::cout<<"F\n";
+
+    return 0;
+}
